d9da7a0a08bd3d6b7cf618c2e371d7846fe39cbd
meg-ai-ml <meghanathalakola@gmail.com>
2026-06-21 23:34:13 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,29 +1,30 @@
-console.log("Hi there!");
-console.log("I am excited to talk to you.");
+console.log("Hi there!"); // Hi there!
+console.log("I am excited to talk to you."); // I am excited to talk 
 
-// Using camelCase for the variables
-let botName = "teacherBot";
+let bot = "teacherBot";
 let botLocation = "the universe";
 
-console.log("Allow me to introduce myself.");
+console.log("Allow me to introduce myself."); // Allow me to introduce myself.
 
-const botIntroduction = "My name is " + bot + ".";
-console.log(botIntroduction);
+const botIntroduction = "My name is " + bot + "."; 
+console.log(botIntroduction); // My name is teacherBot.
 
 const botLocationSentence = "I live in " + botLocation + ".";
-console.log(botLocationSentence);
+console.log(botLocationSentence); // i live in the universe.
 
-// In line 5 , let is used instead const so we can change the string inside the variable 
+// In line 5 , let is used instead of const so we can change the string inside the variable 
 
 bot = "professorBot";
 
 const nicknameIntroduction = "My nickname is " + bot + ".";
-console.log(nicknameIntroduction);
+console.log(nicknameIntroduction); // My nickname is professorBot.
+
+// Note : Bot varible is reassgined to professorBot instead of teacherBot
 
 bot = "awesomeTeacherBot";
 
 let newNicknameGreeting = "I love my nickname but I wish people would call me " + bot + ".";
-console.log(newNicknameGreeting);
+console.log(newNicknameGreeting); // 
 
 
 
