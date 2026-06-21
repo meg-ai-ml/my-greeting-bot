d2c9e1700755d0919be59806dfec8acb35b0cdae
meg-ai-ml <meghanathalakola@gmail.com>
2026-06-21 09:28:19 +0530

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,31 @@
+console.log("Hi there!");
+console.log("I am excited to talk to you.");
+
+// Using camelCase for the variables
+let botName = "teacherBot";
+let botLocation = "the universe";
+
+console.log("Allow me to introduce myself.");
+
+const botIntroduction = "My name is " + bot + ".";
+console.log(botIntroduction);
+
+const botLocationSentence = "I live in " + botLocation + ".";
+console.log(botLocationSentence);
+
+// In line 5 , let is used instead const so we can change the string inside the variable 
+
+bot = "professorBot";
+
+const nicknameIntroduction = "My nickname is " + bot + ".";
+console.log(nicknameIntroduction);
+
+bot = "awesomeTeacherBot";
+
+let newNicknameGreeting = "I love my nickname but I wish people would call me " + bot + ".";
+console.log(newNicknameGreeting);
+
+
+
+
+
